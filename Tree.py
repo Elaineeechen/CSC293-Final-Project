8f7d457262ffaa39fac902367cce64b54ec47e47
Elaine <113189403+Elaineeechen@users.noreply.github.com>
2025-04-28 14:22:53 -0400

    minor changes

--- Tree.py
+++ Tree.py
@@ -1,17 +1,12 @@
 import numpy as np
 import pandas as pd
-from matplotlib.pyplot import subplots
-from statsmodels.datasets import get_rdataset 
 import sklearn.model_selection as skm
-from ISLP import load_data , confusion_table 
 from ISLP.models import ModelSpec as MS
 from sklearn.tree import (DecisionTreeClassifier as DTC, DecisionTreeRegressor as DTR, plot_tree , export_text)
 from sklearn.metrics import (accuracy_score, log_loss) 
-from sklearn.ensemble import \
-(RandomForestRegressor as RF, GradientBoostingRegressor as GBR)
+from sklearn.ensemble import RandomForestClassifier as RFC
 from ISLP.bart import BART
-# import glob
-# import os
+import matplotlib.pyplot as plt
 
 # folder_path = "/Users/elainechen/Downloads"
 # file_list = glob.glob(os.path.join(folder_path, "2023.csv"))
@@ -22,6 +17,7 @@ from ISLP.bart import BART
 
 '''Bagging & Random Forest w/out Lag'''
 weather = pd.read_csv("all_year.csv")
+weather = weather.dropna()
 model = MS(weather.columns.drop(['TIMESTAMP', 'Time of Daily Temp Max', 'Time of Min. Temp', 'Time of Max Wind Spd', 'Time of Min. Wind Spd.', 'Precipitation', 'Is_rain', 'Snow', 'Is_snow', 'Is_anomaly']), intercept=False)
 D = model.fit_transform(weather)
 feature_names = list(D.columns)
@@ -31,7 +27,7 @@ X = np.asarray(D)
 (X_train, X_test, y_train, y_test) = skm.train_test_split(X, weather['Is_anomaly'], test_size = 0.3, random_state = 0)
 
 # Bagging
-bag_weather = RF(max_features = X_train.shape[1], n_estimators = 500, random_state = 0) # Building 500 trees result in a similar MSE (0.04062) when compared to a default of building 100 trees (0.04278)
+bag_weather = RFC(max_features = X_train.shape[1], n_estimators = 500, random_state = 0) # Building 500 trees result in a similar MSE (0.04062) when compared to a default of building 100 trees (0.04278)
 bag_weather.fit(X_train, y_train)
 # ax = subplots(figsize=(8,8))[1] 
 y_hat_bag = bag_weather.predict(X_test) 
@@ -40,15 +36,19 @@ classification_error = np.mean(y_hat_bag != y_test)
 print(f"Bagging (w/out Lag) Classification Error: {classification_error}")
 feature_imp = pd.DataFrame({'importance':bag_weather.feature_importances_}, index = feature_names)
 print(feature_imp.sort_values(by = 'importance', ascending = False))
+one_tree = bag_weather.estimators_[0]
+plt.figure(figsize=(20, 10))
+plot_tree(one_tree, feature_names=feature_names, filled=True, rounded=True, fontsize=10)
+plt.show()
 print()
 
 # Random Forest
-RF_weather = RF(max_features = 4, n_estimators = 500, random_state = 0) 
+RF_weather = RFC(n_estimators = 500, random_state = 0) 
 RF_weather.fit(X_train, y_train)
 # ax = subplots(figsize=(8,8))[1] 
-y_hat_bag = RF_weather.predict(X_test) 
+y_hat_rf = RF_weather.predict(X_test) 
 # ax.scatter(y_hat_bag, y_test) 
-classification_error = np.mean(y_hat_bag != y_test)
+classification_error = np.mean(y_hat_rf != y_test)
 print(f"Random Forest (w/out Lag) Classification Error: {classification_error}")
 feature_imp = pd.DataFrame({'importance':RF_weather.feature_importances_}, index = feature_names)
 print(feature_imp.sort_values(by = 'importance', ascending = False))
@@ -62,6 +62,7 @@ weather['Is_rain_lag3'] = weather['Is_rain'].shift(3)
 weather['Is_snow_lag1'] = weather['Is_snow'].shift(1)
 weather['Is_snow_lag2'] = weather['Is_snow'].shift(2)
 weather['Is_snow_lag3'] = weather['Is_snow'].shift(3)
+weather = weather.dropna()
 model = MS(weather.columns.drop(['TIMESTAMP', 'Time of Daily Temp Max', 'Time of Min. Temp', 'Time of Max Wind Spd', 'Time of Min. Wind Spd.', 'Precipitation', 'Is_rain', 'Snow', 'Is_snow', 'Is_anomaly']), intercept=False)
 D = model.fit_transform(weather)
 feature_names = list(D.columns)
@@ -71,7 +72,7 @@ X = np.asarray(D)
 (X_train, X_test, y_train, y_test) = skm.train_test_split(X, weather['Is_anomaly'], test_size = 0.3, random_state = 0)
 
 # Bagging
-bag_weather = RF(max_features = X_train.shape[1], n_estimators = 500, random_state = 0) # Building 500 trees result in a similar MSE (0.04062) when compared to a default of building 100 trees (0.04278)
+bag_weather = RFC(max_features = X_train.shape[1], n_estimators = 500, random_state = 0) # Building 500 trees result in a similar MSE (0.04062) when compared to a default of building 100 trees (0.04278)
 bag_weather.fit(X_train, y_train)
 # ax = subplots(figsize=(8,8))[1] 
 y_hat_bag = bag_weather.predict(X_test) 
@@ -83,12 +84,12 @@ print(feature_imp.sort_values(by = 'importance', ascending = False))
 print()
 
 # Random Forest
-RF_weather = RF(max_features = 4, n_estimators = 500, random_state = 0) 
+RF_weather = RFC(n_estimators = 500, random_state = 0)
 RF_weather.fit(X_train, y_train)
 # ax = subplots(figsize=(8,8))[1] 
-y_hat_bag = RF_weather.predict(X_test) 
+y_hat_rf = RF_weather.predict(X_test)
 # ax.scatter(y_hat_bag, y_test) 
-classification_error = np.mean(y_hat_bag != y_test)
+classification_error = np.mean(y_hat_rf != y_test)
 print(f"Random Forest (w/ Lag) Classification Error: {classification_error}")
 feature_imp = pd.DataFrame({'importance':RF_weather.feature_importances_}, index = feature_names)
 print(feature_imp.sort_values(by = 'importance', ascending = False))
